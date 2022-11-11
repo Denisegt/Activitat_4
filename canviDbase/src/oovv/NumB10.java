ddec1ee25ae28e24338b1072de2254f58090f330
DeniseGT <dentam.cheste@gmail.com>
2022-11-11 23:19:58 +0100

    Tercer commit

--- canviDbase/src/oovv/NumB10.java
+++ canviDbase/src/oovv/NumB10.java
@@ -12,16 +12,39 @@ public class NumB10 {
 
     private int num;
 
+    /**
+     * constructor de NumB10. número en base 10
+     *
+     * @param num valor del número en base 10
+     */
     public NumB10(int num) {
         this.num = num;
     }
 
+    /**
+     * assigna el valor al número.
+     *
+     * @param num valor a assignar
+     */
     public void setNum(int num) {
         this.num = num;
     }
 
     @Override
     public String toString() {
-        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+        return num; // (error 1)
+    }
+
+    public String canviaABase(int base) {
+        if (base < 2 || base > 24) {
+            throw new ArithmeticException("la base va de 2 a 24");
+        }
+        String digits = "0123456789ABCDEFGHIJKLMN"; // (error 2)
+        String cad = "";
+        while (num > "O") { // (error 3)
+            cad = cad + digits.charAt(num % base);
+            num = num / base;
+        }
+        return alReves(cad); // crida al mètode alReves
     }
 }
